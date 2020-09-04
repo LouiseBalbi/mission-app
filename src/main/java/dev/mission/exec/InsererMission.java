62ea1ef6c8c2bdb55de44db64bf8a0bcc6184d43
LouiseBalbi <67003682+LouiseBalbi@users.noreply.github.com>
2020-09-04 12:04:16 +0200

    TP spring boot en cours

---
+++ src/main/java/dev/mission/exec/InsererMission.java
@@ -0,0 +1,30 @@
+package dev.mission.exec;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import dev.mission.entite.Mission;
+import dev.mission.repository.MissionRepository;
+
+@Controller
+@Profile("insert")
+public class InsererMission implements Runnable {
+	private MissionRepository missionRepository;
+
+	public InsererMission(MissionRepository missionRepository) {
+		super();
+		this.missionRepository = missionRepository;
+	}
+
+	@Override
+	public void run() {
+		Mission mission = new Mission();
+		mission.setLibelle("Mission 5");
+		mission.setTauxJournalier(new BigDecimal("200.5"));
+		mission.setDateDebut(LocalDate.of(2021, 1, 10));
+		mission.setDateFin(LocalDate.of(2022, 1, 4));
+		this.missionRepository.save(mission);
+	}
+}
---
+++ src/main/java/dev/mission/exec/ListerProchainesMissions.java
@@ -0,0 +1,10 @@
+package dev.mission.exec;
+
+import org.springframework.context.annotation.Profile;
+
+
+@Profile("lister")
+public class ListerProchainesMissions {
+
+
+}
